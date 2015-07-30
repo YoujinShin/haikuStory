ef38268fa541cacf9ca41bfa84bf839374abbe3e
Youjin Shin <meggonagul@gmail.com>
2015-07-30 10:59:23 -0400

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+width = $('body').width();
 
+
+// d3.select('#ship').transition().duration(14000)
+// 	.style('left', width/2 + 'px');
+
+d3.select('#duck').transition().duration(20000)
+	// .style('right', width + 200 + 'px');
+	.style('right', width/2 - 80 + 'px');
+
+d3.select('#poem_1').transition().duration(8000)
+	.style('opacity', 1);
+
+d3.select('#line').transition().duration(8000)
+	.style('width', 220 + 'px');
