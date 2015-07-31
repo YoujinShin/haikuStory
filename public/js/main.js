d50637dac7cb5813335c23358fb96d9945994160
Youjin Shin <meggonagul@gmail.com>
2015-07-30 21:13:59 -0400

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,9 @@
 width = $('body').width();
 
-
 // d3.select('#ship').transition().duration(14000)
 // 	.style('left', width/2 + 'px');
 
-d3.select('#duck').transition().duration(20000)
-	// .style('right', width + 200 + 'px');
+d3.select('#duck').transition().duration(8000)
 	.style('right', width/2 - 80 + 'px');
 
 d3.select('#poem_1').transition().duration(8000)
