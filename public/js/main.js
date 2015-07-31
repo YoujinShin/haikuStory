054731bd2b2aea48fa865eb4c31e272bcfbf643d
Youjin Shin <meggonagul@gmail.com>
2015-07-31 00:53:21 -0400

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,34 @@
 width = $('body').width();
-height = window.innerHeight;	
+height = window.innerHeight;
+
+first_draw = true;
+second_draw = false;
+third_draw = false;
 
 
 $(function(){
 
-	width = $('body').width();
-	height = window.innerHeight;
+	firstPoem();
 
-	// firstPoem();
-	secondPoem();
-	// thirdPoem();
+	setTimeout(function () {
+    	d3.select('#arrow').transition().duration(2000)
+		.style('opacity', 1);
+    }, 10000);
 });
 
 
 function firstPoem() {
 
 	console.log('first poem');
+	// selectBttn1();
+	first_draw = true;
 
-	$('.img_1').css('visibility', 'visible');
-	$('.img_2').css('visibility', 'hidden');
-	$('.img_3').css('visibility', 'hidden');
+	$('#img_1').css('visibility', 'visible');
+	$('#img_2').css('visibility', 'hidden');
+	$('#img_3').css('visibility', 'hidden');
+
+	$('#box').css('visibility', 'hidden');
+	$('#box2').css('visibility', 'hidden');
 
 	d3.select('#duck').transition().duration(8000)
 		.style('right', width/2 - 80 + 'px');
@@ -34,18 +43,19 @@ function firstPoem() {
 function secondPoem() {
 
 	console.log('second poem');
+	d3.select('#arrow').transition().duration(2000)
+		.style('opacity', 0);
+	// selectBttn2();
+	second_draw = true;
 
-	d3.select('#img_2').style('visibility', 'visible');
 	$('#img_1').css('visibility', 'hidden');
-	// $('.img_2').css('visibility', 'visible');
-	// $('.img_3').css('visibility', 'hidden');
+	$('#img_2').css('visibility', 'visible');
+	$('#img_3').css('visibility', 'hidden');
 
-	
-	d3.select('#bg').style('visibility', 'visible');
-	d3.select('#bg').style('visibility', 'hidden');
+	$('#box2').css('visibility', 'hidden');
+	$('#box').css('visibility', 'visible');
 
-
-	d3.select('#box').transition().duration(18000)
+	d3.select('#box').transition().duration(14000)
 		.style('left', width + 400 + 'px');
 
 	setTimeout(function () {
@@ -57,14 +67,17 @@ function secondPoem() {
 function thirdPoem() {
 
 	console.log('third poem');
+	// selectBttn3();
+	third_draw = true;
 
 	$('#img_1').css('visibility', 'hidden');
 	$('#img_2').css('visibility', 'hidden');
 	$('#img_3').css('visibility', 'visible');
 
-	console.log(  $('#img_3').css("visibility")  );
+	$('#box').css('visibility', 'hidden');
+	$('#box2').css('visibility', 'visible');
 
-	d3.select('#box2').transition().duration(8000)
+	d3.select('#box2').transition().duration(8800)
 		.style('bottom', height + 'px');
 
 	setTimeout(function () {
@@ -72,6 +85,128 @@ function thirdPoem() {
 		.style('opacity', 1);
     }, 4000);
 }
+
+
+// scroll
+var topMax = $('body').height() - window.innerHeight;
+
+onscroll = function() {
+
+  scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if(scrollTop < topMax/3) { 
+  	if(first_draw == false) { 
+  		firstPoem(); 
+  		selectBttn1();
+  		second_draw = false;
+  		third_draw = false;
+  	}
+  } else if(scrollTop > topMax/3  && scrollTop < topMax*2/3) {
+  	if(second_draw == false) { 
+  		secondPoem(); 
+  		selectBttn2();
+  		first_draw = false;
+  		third_draw = false;
+  	}
+  } else if(scrollTop > topMax*2/3) {
+  	if(third_draw == false) { 
+  		thirdPoem(); 
+  		selectBttn3();
+  		first_draw = false;
+  		second_draw = false;
+  	}
+  }
+};
+
+
+function selectBttn1() {
+	$('#bttn_1').css('margin-top', 0 + 'px');
+	$('#bttn_1').css('height', 2 + 'px');
+
+	d3.select('#bttn_1').transition().duration(0)
+		.style('height', 8 + 'px');
+
+	$('#bttn_2').css('margin-top', 0 + 'px');
+	$('#bttn_2').css('height', 2 + 'px');
+
+	$('#bttn_3').css('margin-top', 0 + 'px');
+	$('#bttn_3').css('height', 2 + 'px');
+}
+
+function selectBttn2() {
+	$('#bttn_1').css('margin-top', 0 + 'px');
+	$('#bttn_1').css('height', 2 + 'px');
+
+	$('#bttn_2').css('margin-top', 0 + 'px');
+	$('#bttn_2').css('height', 2 + 'px');
+
+	d3.select('#bttn_2').transition().duration(0)
+		.style('height', 8 + 'px');
+
+	$('#bttn_3').css('margin-top', 0 + 'px');
+	$('#bttn_3').css('height', 2 + 'px');
+}
+
+function selectBttn3() {
+	$('#bttn_1').css('margin-top', 0 + 'px');
+	$('#bttn_1').css('height', 2 + 'px');
+
+	$('#bttn_2').css('margin-top', 0 + 'px');
+	$('#bttn_2').css('height', 2 + 'px');
+
+	$('#bttn_3').css('margin-top', 0 + 'px');
+	$('#bttn_3').css('height', 2 + 'px');
+
+	d3.select('#bttn_3').transition().duration(0)
+		.style('height', 8 + 'px');
+}
+
+
+
+
+
+
+// function selectBttn1() {
+// 	$('#bttn_1').css('margin-top', 6 + 'px');
+// 	$('#bttn_1').css('height', 2 + 'px');
+
+// 	d3.select('#bttn_1').transition().duration(100)
+// 		.style('height', 8 + 'px');
+
+// 	$('#bttn_2').css('margin-top', 6 + 'px');
+// 	$('#bttn_2').css('height', 2 + 'px');
+
+// 	$('#bttn_3').css('margin-top', 6 + 'px');
+// 	$('#bttn_3').css('height', 2 + 'px');
+// }
+
+// function selectBttn2() {
+// 	$('#bttn_1').css('margin-top', 6 + 'px');
+// 	$('#bttn_1').css('height', 2 + 'px');
+
+// 	$('#bttn_2').css('margin-top', 0 + 'px');
+// 	$('#bttn_2').css('height', 8 + 'px');
+
+// 	// d3.select('#bttn_2').transition().duration(100)
+// 	// 	.style('height', 8 + 'px');
+
+// 	$('#bttn_3').css('margin-top', 6 + 'px');
+// 	$('#bttn_3').css('height', 2 + 'px');
+// }
+
+// function selectBttn3() {
+// 	$('#bttn_1').css('margin-top', 6 + 'px');
+// 	$('#bttn_1').css('height', 2 + 'px');
+
+// 	$('#bttn_2').css('margin-top', 6 + 'px');
+// 	$('#bttn_2').css('height', 2 + 'px');
+
+// 	$('#bttn_3').css('margin-top', 0 + 'px');
+// 	$('#bttn_3').css('height', 8 + 'px');
+
+// 	// d3.select('#bttn_3').transition().duration(100)
+// 	// 	.style('height', 8 + 'px');
+// }
 
 
 
